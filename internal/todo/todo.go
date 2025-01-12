c6ec99111d49f19aa1c05a08596585bdfb1c04ff
Joshua <joshua@Joshuas-MacBook-Pro-2.local>
2025-01-12 10:58:59 +0530

    complete lesson 2

--- internal/todo/todo.go
+++ internal/todo/todo.go
@@ -2,27 +2,32 @@ package todo
 
 import "errors"
 
+type Item struct {
+	Task   string
+	Status string
+}
+
 type Service struct {
-	todo []string
+	todo []Item
 }
 
 // constructor helps to access the variable
 func NewService() *Service {
 	return &Service{
-		todo: make([]string, 0),
+		todo: make([]Item, 0),
 	}
 }
 
 func (svc *Service) Add(s string) error {
 	for _, v := range svc.todo {
-		if v == s {
+		if v.Task == s {
 			return errors.New("Duplicate entry is not allowed")
 		}
 	}
-	svc.todo = append(svc.todo, s)
+	svc.todo = append(svc.todo, Item{Task: s, Status: "TO BE DONE"})
 	return nil
 }
 
-func (svc *Service) GetAll() []string {
+func (svc *Service) GetAll() []Item {
 	return svc.todo
 }
